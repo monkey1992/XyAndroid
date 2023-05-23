658acc8b039a77e53a04354f0737c59a1c224cc4
monkey1992 <909923653@qq.com>
2023-05-23 18:46:37 +0800

    EditText

--- sdk/src/main/java/com/xy/android/sdk/view/edittext/EditTextActivity.kt
+++ sdk/src/main/java/com/xy/android/sdk/view/edittext/EditTextActivity.kt
@@ -2,6 +2,7 @@ package com.xy.android.sdk.view.edittext
 
 import android.os.Bundle
 import android.text.InputType
+import android.text.method.NumberKeyListener
 import com.xy.android.sdk.BackActivity
 import com.xy.android.sdk.databinding.ActivityEditTextBinding
 
@@ -32,5 +33,61 @@ class EditTextActivity : BackActivity() {
         // 负数
         binding.editTextCodeSigned.inputType =
             InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_FLAG_SIGNED
+
+        // KeyListener 实现
+        // 数字
+        binding.editTextKeyListenerNumber.keyListener = object : NumberKeyListener() {
+            override fun getInputType(): Int {
+                // 返回 InputType
+                return InputType.TYPE_CLASS_NUMBER
+            }
+
+            override fun getAcceptedChars(): CharArray {
+                // 返回允许输入的内容
+                return charArrayOf('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+            }
+        }
+
+        // 小数
+        binding.editTextKeyListenerDecimal.keyListener = object : NumberKeyListener() {
+            override fun getInputType(): Int {
+                return InputType.TYPE_CLASS_NUMBER
+            }
+
+            override fun getAcceptedChars(): CharArray {
+                return charArrayOf('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.')
+            }
+        }
+
+        // 负数
+        binding.editTextKeyListenerSigned.keyListener = object : NumberKeyListener() {
+            override fun getInputType(): Int {
+                return InputType.TYPE_CLASS_NUMBER
+            }
+
+            override fun getAcceptedChars(): CharArray {
+                return charArrayOf('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-')
+            }
+        }
+
+        // 字母
+        binding.editTextKeyListenerLetter.keyListener = object : NumberKeyListener() {
+            override fun getInputType(): Int {
+                return InputType.TYPE_CLASS_TEXT
+            }
+
+            override fun getAcceptedChars(): CharArray {
+                return charArrayOf(
+                    'A', 'B', 'C', 'D', 'E', 'F', 'G',
+                    'H', 'I', 'J', 'K', 'L', 'M', 'N',
+                    'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+                    'V', 'W', 'X', 'Y', 'Z',
+                    'a', 'b', 'c', 'd', 'e', 'f', 'g',
+                    'h', 'i', 'j', 'k', 'l', 'm', 'n',
+                    'o', 'p', 'q', 'r', 's', 't', 'u',
+                    'v', 'w', 'x', 'y', 'z',
+                )
+            }
+        }
     }
 }
