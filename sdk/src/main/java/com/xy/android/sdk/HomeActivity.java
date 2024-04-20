7c1da7aa13b979cde153daee2af43f6fe1d6d611
monkey1992 <909923653@qq.com>
2024-04-20 15:03:17 +0800

    Android 动画

---
+++ sdk/src/main/java/com/xy/android/sdk/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.xy.android.sdk;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class HomeActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        assert true : "xxxxxx";
+    }
+}
